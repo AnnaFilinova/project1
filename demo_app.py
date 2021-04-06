28eb21bbdd60b3809b526c093613b19275ce5d8c
AnnaFilinova <afilinova@nes.ru>
2021-04-06 18:07:57 +0300

    1 try

--- demo_app.py
+++ demo_app.py
@@ -6,11 +6,13 @@ import pandas as pd
 with st.echo(code_location='below'):
     df=pd.read_csv("netflix_titles.csv")
     st.title("Netflix Movies and TV Shows")
-    """
-    This is a test.
-    """
-    x = np.linspace(0, 10, 500)
-    fig = plt.figure()
-    plt.plot(x, np.sin(x))
-    plt.ylim(-2, 2)
-    st.pyplot(fig)
+    date1 = st.slider('The Starting Year of the Interval', min_value=1925, max_value=2021)
+    date2= st.slider('The Ending Year of the Interval', min_value=date1, max_value=2021)
+    index=[i for i in range (date1, date2+1)]
+    values=[ sum(df['release_year']==i) for i in range (date1, date2+1)]
+    fig, ax = plt.subplots()
+    plt.title(f"Number of Movies and TV Shows Released Between {date1} and {date2}")
+    ax.bar(index, values)
+    ax.set_xlabel("Year")
+    ax.set_ylabel("Number of Movies and TV Shows Released")
+    plt.show()
