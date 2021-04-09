3b2fa0de96e573744e614f926ab3ab13e8610653
AnnaFilinova <afilinova@nes.ru>
2021-04-10 00:40:19 +0300

    1 try

--- demo_app.py
+++ demo_app.py
@@ -56,7 +56,7 @@ with st.echo(code_location='below'):
     s = BeautifulSoup(r.text)
     pstr = s.find_all('img')[1]['src']
     st.write(f'Poster for {name}')
-    st.image(pstr)
-    st.balloons
+    st.image(pstr, zoom=5)
+    st.balloons()
 
 
