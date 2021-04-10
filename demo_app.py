865f778520593f2063a668bb634b83b168d83c42
AnnaFilinova <afilinova@nes.ru>
2021-04-10 16:53:22 +0300

    1 try

--- demo_app.py
+++ demo_app.py
@@ -68,7 +68,10 @@ with st.echo(code_location='below'):
     contents = file_.read()
     data_url = base64.b64encode(contents).decode("utf-8")
     file_.close()
-
+    st.markdown(
+        f'<img src="data:image/gif;base64,{data_url}" alt=f'Posters for {name}'>',
+        unsafe_allow_html=True,
+    )
     #st.write(f'Poster for {name}')
     #st.image(pstr, width=250)
     #st.balloons()
