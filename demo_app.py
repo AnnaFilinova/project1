38c2b03cffe3bc130cbe32e5a722e942e12d50d7
AnnaFilinova <afilinova@nes.ru>
2021-04-11 16:39:48 +0300

    1 try

--- demo_app.py
+++ demo_app.py
@@ -39,14 +39,14 @@ with st.echo(code_location='below'):
     df1['proportion']=1
     df1=df1[['release_year', 'proportion','type' ]]
     df1=df1[df1['release_year']>=1980]
-    prop=alt.Chart(df1, title=f'Movies/TV Shows proportion in {country} in 1980-2021').mark_area().encode(
+    prop=alt.Chart(df1, title=f'Movies/TV Shows proportion in {country} in 1980-2021').mark_bar().encode(
         x=alt.X('release_year'),
-        y=alt.Y('proportion', stack="normalize"),
+        y=alt.Y("count(proportion)", stack="normalize"),
         color='type'
     ).properties(
         width=700,
         height=500
-    )
+    ).tooltip=[alt.Tooltip('count(proportion)', title="Total Students")]
     st.altair_chart(prop)
 
     st.subheader("Would you like some posters?")
